c138196dbf7facf63285f314c241d43cda3b223e
Manish Tripathy <manish.tripathy@outlook.com>
2019-05-06 07:51:38 +0530

    [Thingy] Copy initial setup from Ankita

---
+++ src/main/java/com/thingy/controller/FrontendController.java
@@ -0,0 +1,13 @@
+package com.thingy.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FrontendController {
+
+    @GetMapping("/hello")
+    public String sayHello(){
+        return "Hello World";
+    }
+}
